1927d67f387e694de88aec09d5f86664c9421f96
avikonduru <avikonduru@gmail.com>
2025-02-16 12:43:56 -0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,10 @@ import {
 	Button,
 	Flex,
 	IconButton,
+	Separator,
+	Text,
+	Link,
+	Card,
 } from '@chakra-ui/react';
 import { FaXTwitter } from 'react-icons/fa6';
 
@@ -22,16 +26,52 @@ const HomePage = () => {
 			>
 				<Container maxWidth="5xl">
 					<Flex align="center">
-						<Heading size="2xl">Bounty Super Staker</Heading>
+						<Heading size="xl">bounty_super_staker</Heading>
 						<Spacer />
-						{/* <IconButton
-							aria-label="X Link"
-							variant="surface"
-							mr={4}
-						>
-							<FaXTwitter />
-						</IconButton> */}
 						<Button variant="surface">Connect Wallet</Button>
+					</Flex>
+				</Container>
+			</Box>
+			<Box py="8">
+				<Container maxWidth="3xl">
+					<Card.Root variant={'subtle'}>
+						<Card.Body>
+							<Box>hihi</Box>
+						</Card.Body>
+					</Card.Root>
+				</Container>
+			</Box>
+			<Box
+				width="100%"
+				py="4"
+			>
+				<Container maxWidth="5xl">
+					<Flex
+						direction="column"
+						width="100%"
+						gap="4"
+					>
+						<Separator />
+						<Flex align="center">
+							<Heading size="xl">bounty_super_staker</Heading>
+							<Spacer />
+							<IconButton
+								aria-label="X Link"
+								variant="ghost"
+								mr={4}
+							>
+								<FaXTwitter />
+							</IconButton>
+						</Flex>
+						<Flex
+							align="center"
+							justify="center"
+							gap="4"
+						>
+							<Link variant="underline">Privacy Policy</Link>
+							<Link variant="underline">Terms of Service</Link>
+							<Text>&copy; 2025 Realm Labs</Text>
+						</Flex>
 					</Flex>
 				</Container>
 			</Box>
